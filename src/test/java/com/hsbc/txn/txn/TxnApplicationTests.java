13f605d0d1b1c1768162ebc1f8d4834d279b02a6
zihuafeng <fengzihua54@163.com>
2025-06-15 17:42:24 +0800

    提交

---
+++ src/test/java/com/hsbc/txn/txn/TxnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsbc.txn.txn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TxnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
